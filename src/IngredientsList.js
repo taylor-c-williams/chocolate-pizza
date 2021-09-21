98adc5196080123bcf3d9ad3ad6fb5252e81c322
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-20 17:34:34 -0700

    CPd package.json from react bootstrap

--- src/IngredientsList.js
+++ src/IngredientsList.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export default class IngredientsList extends Component {
   render() {
     return (
-      <div>
+      <div className = "Card">
         
         <ul>
           <li>
